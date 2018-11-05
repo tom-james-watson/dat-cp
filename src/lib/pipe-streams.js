a1358b479290f3311d695488e58a35ffc90a65c3
Tom Watson <tom@tomjwatson.com>
2018-11-06 00:07:55 +0100

    make progress bar be 2/3 label 1/3 bar

--- src/lib/pipe-streams.js
+++ src/lib/pipe-streams.js
@@ -2,14 +2,17 @@ import ProgressBar from 'progress'
 
 export default function pipeStreams(readStream, writeStream, filesize, label) {
   return new Promise(async (resolve) => {
-    const width = process.stdout.columns < 100 ? process.stdout.columns - 30 : 50
-    label = label.substr(0, width).padEnd(width, ' ')
+
+    const labelWidth = ((process.stdout.columns / 3) * 2 - 5).toFixed()
+    const barWidth = (process.stdout.columns / 3 - 5).toFixed()
+
+    label = label.substr(0, labelWidth).padEnd(labelWidth, ' ')
 
     const bar = new ProgressBar(
       `${label} [:bar] :percent`,
       {
         incomplete: ' ',
-        width: 20,
+        width: barWidth,
         total: filesize
       }
     )
