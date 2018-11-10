6e324e914ba252e057b20669c0d69bdb4dcda7e4
Tom Watson <tom@tomjwatson.com>
2018-11-10 15:16:41 +0100

    make download sparse, otherwise files are downloaded immediately

--- src/receive.js
+++ src/receive.js
@@ -1,7 +1,7 @@
 import DatCp from './lib/dat-cp'
 
 export default async function receive(key, program) {
-  const datCp = new DatCp(program, {key})
+  const datCp = new DatCp(program, {key, sparse: true})
   await datCp.connect()
   await datCp.download()
   process.exit()
